c7deea0b5c6afe21b688b02c010ccc931e81b793
Eugenio Jesus <natanael.unicid@outlook.com>
2024-10-01 09:59:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,73 @@
+let dados = [
+    {
+        titulo: "The Walking Dead",
+        classificacao:"2010 | Classificação etária:A18 | 11 temporadas | Terror" ,
+        descricao: "A trama se desenrola em um mundo devastado por um surto zumbi, onde um grupo de sobreviventes luta para se manter vivo em meio à escassez de recursos, perigos externos e, muitas vezes, conflitos internos.",
+        link: "https://pt.wikipedia.org/wiki/The_Walking_Dead_(s%C3%A9rie_de_televis%C3%A3o)",
+         tags:"twd zumbi apocalypse amc  Rick Grimes  Daryl Dixon  michonne maggie green carl negan shane andrea ezequiel beth abraham carol"
+    },
+    {
+        titulo: "La casa de papel",
+        classificacao:"2017 | Classificação etária:A16 | 5 temporadas | Suspense" ,
+        descricao: "A série narra a história de um grupo de ladrões altamente qualificados, liderados pelo misterioso Professor, que planejam um dos maiores roubos da história",
+        link: "https://pt.wikipedia.org/wiki/La_casa_de_papel",
+        tags: "espanha banco  dinheiro professor berlim tóquio rio denver nairobi moscou oslo marsella arturo bogotá alicia"
+    },
+    {
+        titulo: "The Rain",
+        classificacao:" 2018 | Classificação etária:A16 | 3 temporadas | Ficção científica" ,
+        descricao: "The Rain é uma série de televisão dinamarquesa que nos transporta para um futuro próximo devastado por um vírus mortal transmitido pela chuva",
+        link: "https://pt.wikipedia.org/wiki/The_Rain_(s%C3%A9rie_de_televis%C3%A3o)",
+         tags: "chuva apocalypse virus rasmus simone martin patrick lea jean beatrice ellen frederick"
+    },
+    {
+        titulo: "Cobra Kai",
+        classificacao:"2018 | Classificação etária:14 | 6 temporadas | Ação" ,
+        descricao: "Cobra Kai é uma série de televisão de drama e artes marciais que serve como continuação da icônica franquia Karate Kid. A série se passa décadas após os eventos dos filmes originais e acompanha a rivalidade entre os antigos adversários, Johnny Lawrence e Daniel LaRusso.",
+        link: "https://pt.wikipedia.org/wiki/Cobra_Kai",
+        tags: "karate luta karatekid rivalidade daniel larusso jhonny samantha miguel robby tory jhon kreese terry amanda "
+    },
+    {
+        titulo: "Game of Thrones",
+        classificacao:"2011 | Classificação etária:16 | 8 temporadas | Fantasia & Ficção Científica" ,
+        descricao: "É uma épica série de fantasia medieval que conquistou o mundo com sua trama complexa, personagens cativantes e um universo rico em detalhes. Adaptada da série de livros A Song of Ice and Fire, de George R.R. Martin.",
+        link: "https://pt.wikipedia.org/wiki/Game_of_Thrones",
+        tags: "dragões  dragao fantasia reino  Stark Lannister Targaryen Baratheon Sansa Stark Arya Stark Bran Stark Hodor  bran stark "
+    },
+    {
+        titulo: "Stranger Things",
+        classificacao:"2016 | Classificação etária:A16 | 4 temporadas | Ficção científica" ,
+        descricao: "É uma série de televisão americana que nos transporta para a década de 1980, em uma pequena cidade americana chamada Hawkins. A trama se desenrola em torno do desaparecimento misterioso de um garoto chamado Will Byers e dos eventos sobrenaturais que se seguem",
+        link: "https://pt.wikipedia.org/wiki/Stranger_Things",
+        tags: "anos80 terror mundoinvertido suspense   onze mike wheeler dustin will max lucas joyce nancy steve robin eddie vecna billy barb hollan"
+         
+    },
+    {
+        titulo: "Outer Banks",
+        classificacao:"2020 | Classificação etária:A16 | 3 temporadas | Drama" ,
+        descricao: "É uma série de televisão americana que nos transporta para as belas, porém perigosas, praias da Carolina do Norte. A trama gira em torno de um grupo de adolescentes, conhecidos como Pogues,que vivem na parte mais pobre da ilha e sonham com uma vida melhor.",
+        link: "https://pt.wikipedia.org/wiki/Outer_Banks_(s%C3%A9rie_de_televis%C3%A3o)",
+          tags: "surf praia amizade verão  johnB sara cameron kiara pope jj rafe ward topper"
+    },
+    {
+        titulo: "On My Block",
+        classificacao:"2018 | Classificação etária:A14 | 4 temporadas | Comédia",
+        descricao:"É uma série de comédia dramática americana que nos leva para as ruas de um bairro de Los Angeles, nos Estados Unidos. A série acompanha um grupo de quatro adolescentes que navegam pelos desafios da adolescência, da amizade e da vida em um bairro com altas taxas de criminalidade.",
+        link: "https://pt.wikipedia.org/wiki/On_My_Block_(s%C3%A9rie_de_televis%C3%A3o)",
+        tags: "gangues escola amor comédia cesar diaz monse jamal ruby spooky olivia abuela"
+    },
+    {
+        titulo: "Lost",
+        classificacao:"2004 | Classificação etária:A16 | 6 temporadas | Ficção científica",
+        descricao:"É uma série de televisão americana que nos transporta para uma ilha tropical misteriosa, onde um grupo de sobreviventes de um acidente aéreo luta para sobreviver. A trama, repleta de mistérios, suspense e elementos sobrenaturais, cativou milhões de espectadores ao redor do mundo.",
+        link: "https://pt.wikipedia.org/wiki/Lost_(s%C3%A9rie_de_televis%C3%A3o)",
+        tags: "ilha sobreviventes mistério avião jack kate john james hugo sayid sun jin claire"
+    },
+    {
+        titulo: "Sombra e Ossos",
+        classificacao:"2021 | Classificação etária:A16 | 2 temporadas | Fantasia ",
+        descricao:"É uma série de fantasia épica adaptada da duologia de livros de Leigh Bardugo, que nos transporta para o mundo sombrio e mágico de Ravka. A história se passa em um reino dividido por uma vasta e escura dobra, habitada por criaturas monstruosas.",
+        link: "https://pt.wikipedia.org/wiki/Shadow_and_Bone_(s%C3%A9rie_de_televis%C3%A3o)",
+        tags: "magia fantasia guerra alina general kirigan genya zoya kaz nina wylan baghra matthias nicolai tamar tolya"
+    }
+];
